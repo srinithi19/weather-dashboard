c5d4364f6e4af7eef04974923277b6f8dc761c5a
Srinithi Ravichandran <emailtonithi@gmail.com>
2023-01-10 21:09:11 -0800

    implement five day forecase method

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,35 +26,32 @@ function displaySearchedCity() {
 displaySearchedCity();
 
 //function to make api call to retrieve lat & lon of respective city and make current weather and fivedayweather ap call
- function fetchWeather(city) {
+function fetchWeather(city) {
     let coords = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
     fetch(coords)
     .then(function(response){return response.json()})
     .then(function(data){ 
-        console.log(data);
+        //console.log(data);
         let lat = data[0].lat;
         let lon = data[0].lon;
-        console.log(lat,lon,apiKey,units);
+        //console.log(lat,lon,apiKey,units);
         currentWeather(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`);
-        //forecastWeather(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        forecastWeather(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`);
         inputEl.value = "";
-        $(".futureForecast").html("");
-    
+        $(".futureForecast").html("");    
     });
 }
 
-// function to make server side api call and display current weather
+//function to make server side api call and display current weather
 function currentWeather(currentUrl) {
     fetch(currentUrl)
     .then(function(response){return response.json()})
     .then(function(data){ 
         console.log(data);
         let currentDate = new Date(data.dt * 1000);
-        let text = "Current Weather";
         $(".currentWeather").attr("style", "border: 1px solid black");
         $(".currentWeather").html(
-            `<h2>${text}</h2><br>
-            <h3>${data.name} (${currentDate.toLocaleDateString("en-US")})</h3> 
+            `<h3>${data.name} (${currentDate.toLocaleDateString("en-US")})</h3> 
             <figure>
             <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"  alt="icon showing the weather">
             </figure>
@@ -65,26 +62,53 @@ function currentWeather(currentUrl) {
     });        
 }
 
+// function to make server side api call and display forescast weather for next 5 days
+function forecastWeather(forecastUrl) {
+    fetch(forecastUrl)
+    .then(function(response){return response.json()})
+    .then(function(data){ 
+        console.log(data);
+        let text = "5 Day Forecast:";
+        $("#future").text("5 day forecast:");
+        //use an array as per 3 hr forecase to retrive 5 days
+        let dayArr = [0, 8, 16, 24, 32];
+        //loop over the array and create a div for each day with the weather conditions as list items
+        dayArr.forEach(function (i) {
+        let futureDate = new Date(data.list[i].dt * 1000);
+        futureDate = futureDate.toLocaleDateString("en-US");
+        $(".futureForecast").append(`
+        <div>${futureDate}
+        <figure>
+        <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png" alt="icon showing the weather">
+        </figure>
+         <ul><li>Temp: ${data.list[i].main.temp} \u2109</li>
+         <li>Wind: ${data.list[i].wind.speed} MPH</li>
+         <li>Humidity: ${data.list[i].main.humidity}%</li>
+         </ul></div`
+        );       
+        })           
+    });        
+}
+  
+//clear local storage
+$("#clear").on("click", function () {
+    localStorage.clear();
+    $("#storedCity").html("");
+    location.reload();
+});
     
-    //clear local storage
-    $("#clear").on("click", function () {
-        localStorage.clear();
-        $("#storedCity").html("");
-        location.reload();
-        return false;
-    });
-    
-    //event listener and get city from user input or saved cities
-    submitFormEl.addEventListener("click", function () {
-        city = inputEl.value.trim();
-        if (city === "") {
-            return;
-        }
-        //store the searched city in an array in localStorage
-        if (storedCity.indexOf(city) === -1) {
-            storedCity.push(city);
-            localStorage.setItem("storedCity", JSON.stringify(storedCity));
-            displaySearchedCity();
-        }
-        fetchWeather(city);
-    });
+//event listener and get city from user input or saved cities
+submitFormEl.addEventListener("click", function () {
+    city = inputEl.value.trim();
+    if (city === "") {
+        alert("Please enter city");
+        return;
+    }
+    //store the searched city in an array in localStorage
+    if (storedCity.indexOf(city) === -1) {
+        storedCity.push(city);
+        localStorage.setItem("storedCity", JSON.stringify(storedCity));
+        displaySearchedCity();
+    }
+    fetchWeather(city);
+});
